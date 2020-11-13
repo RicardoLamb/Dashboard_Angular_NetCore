21aff623a19730911b0974688d85631ef49c2c9e
rickrdoaugusto <rickrdoaugusto@gmail.com>
2020-11-13 17:43:23 -0300

    Add server component and setting it.

--- dashboardAngular/src/app/server/server.component.ts
+++ dashboardAngular/src/app/server/server.component.ts
@@ -10,9 +10,31 @@ export class ServerComponent implements OnInit {
 
   constructor() { }
 
+  color: string;
+  buttonText: string;
+
   @Input() serverInput: Server;
 
   ngOnInit(): void {
+    this.getServerStatus(this.serverInput.isOnline);
+  }
+
+  getServerStatus(isOnline: boolean): void {
+    if(isOnline) {
+      this.serverInput.isOnline = true;
+      this.color = '#66BB6A';
+      this.buttonText = 'Shut Down';
+    } else
+    {
+      this.serverInput.isOnline = false;
+      this.color = '#FF686B';
+      this.buttonText = 'Start';
+    }
+  }
+
+  toggleStatus(onlineStatus: boolean): void {
+    console.log(this.serverInput.name, ' : ', onlineStatus);
+    this.getServerStatus(!onlineStatus);
   }
 
 }
